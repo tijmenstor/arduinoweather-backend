9801acee29b6b33a36f59042b4e8b0dae3d277bb
Tijmen34 <23655598+Tijmen34@users.noreply.github.com>
2018-12-11 17:10:41 +0100

    DB is now a seperate class. Authenticates at first.

--- src/database/sequelize.ts
+++ src/database/sequelize.ts
@@ -1,17 +1,50 @@
 import { Sequelize } from "sequelize-typescript";
-import {BMP280} from "../model/BMP280";
-import {DHT11} from "../model/DHT11";
-import {DS18B20} from "../model/DS18B20";
-import {Openweather} from "../model/Openweather";
+import { BMP280 } from "../model/BMP280";
+import { DHT11 } from "../model/DHT11";
+import { DS18B20 } from "../model/DS18B20";
+import { Openweather } from "../model/Openweather";
 
-export const sequelize = new Sequelize({
-  database: 'internetofthings',
-  dialect: 'mysql',
-  username: 'iot',
-  password: 'iotiscool',
-  url: 'localhost',
-  port: 3306,
-  modelPaths: [__dirname + '../model']
-})
+// export const sequelize = new Sequelize({
+//   database: 'internetofthings',
+//   dialect: 'mysql',
+//   username: 'iot',
+//   password: 'iotiscool',
+//   url: 'localhost',
+//   port: 3306,
+//   modelPaths: [__dirname + '../model']
+// })
+//
+// sequelize.addModels([BMP280,DHT11,DS18B20,Openweather])
 
-sequelize.addModels([BMP280,DHT11,DS18B20,Openweather])
+export class sequelizeRepo {
+  repository: Sequelize;
+
+  constructor() {
+    this.config();
+    this.checkConnection();
+  }
+
+  private config() {
+    this.repository = new Sequelize({
+      database: 'internetofthings',
+      dialect: 'mysql',
+      username: 'iot',
+      password: 'iotiscool',
+      url: 'localhost',
+      port: 3306,
+      modelPaths: ['../model']
+    })
+    this.repository.addModels([BMP280, DHT11, DS18B20, Openweather])
+  }
+
+  private checkConnection() {
+    this.repository.authenticate()
+      .then(_ => {
+        console.log("Connection with DB instantiated.");
+      })
+      .catch(err => {
+        console.log("Failed to connect to DB ", err);
+      })
+  }
+}
+new sequelizeRepo();
