80ea6c221f66f8a5efd525c1e97cffa2a46a6eff
Tijmen34 <23655598+Tijmen34@users.noreply.github.com>
2018-12-11 17:10:56 +0100

    Forgot...

--- src/database/sequelize.ts
+++ src/database/sequelize.ts
@@ -4,18 +4,6 @@ import { DHT11 } from "../model/DHT11";
 import { DS18B20 } from "../model/DS18B20";
 import { Openweather } from "../model/Openweather";
 
-// export const sequelize = new Sequelize({
-//   database: 'internetofthings',
-//   dialect: 'mysql',
-//   username: 'iot',
-//   password: 'iotiscool',
-//   url: 'localhost',
-//   port: 3306,
-//   modelPaths: [__dirname + '../model']
-// })
-//
-// sequelize.addModels([BMP280,DHT11,DS18B20,Openweather])
-
 export class sequelizeRepo {
   repository: Sequelize;
 
